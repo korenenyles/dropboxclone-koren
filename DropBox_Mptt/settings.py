884570d5a5ef518d8e34adca72027f0e2f5af31c
korenenyles <korenenyles@gmail.com>
2020-07-07 14:10:28 -0400

    fixed issue with settings

--- DropBox_Mptt/settings.py
+++ DropBox_Mptt/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'DropBox_Mptt.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.join.path(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
