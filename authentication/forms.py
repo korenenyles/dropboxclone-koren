1507e10a9db9e9614c41b5cf7785f71719963e7d
Mustafa Prodhan <mustafa.prodhan.317@gmail.com>
2020-07-14 09:12:15 -0400

    I was able to make the login page and the signup page work, without the password confirmation piece. For the moment, this is the minimum that works. However, need some help with the logic or confirming the logic for password confirmation during signup. Added the logout buttton and that works.

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(max_length=30, unique=True)
+    email = forms.EmailField(max_length=30)
     password = forms.CharField(widget=forms.PasswordInput)
 
 class SignUpForm(forms.Form):
-    email = forms.EmailField(max_length=30, unique=True)
+    email = forms.EmailField(max_length=30)
     password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.Charfield(label='Matching Password', 
-                                        widget=forms.PasswordInput)
+    # confirm_password = forms.CharField(label='Matching Password',
+                                        # widget=forms.PasswordInput)
+    username = forms.CharField(max_length=30)
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from authentication.forms import LoginForm, SignUpForm
+from django.views.generic import View
+from dropbox_user.models import DropBoxUser
 
-# Create your views here.
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, "general_form.html", {"form": form})
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request,
+                                email=data["email"],
+                                password=data["password"]
+            )
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(request.GET.get('next', reverse("landingpage")))
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return HttpResponseRedirect(reverse("loginpage"))
+
+class SignUpView(View):
+    def get(self, request):
+        form = SignUpForm()
+        return render(request, "general_form.html", {"form": form})
+    
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = DropBoxUser.objects.create_user(
+                    email = data["email"],
+                    password = data["password"],
+                    # confirm_password = data["confirm_password"],
+                    username = data["username"]
+            )
+            # if password and confirm_password and password != confirm_password:
+            #     raise forms.ValidationError('Passwords do not match')
+            # return confirm_password
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(reverse("loginpage"))
