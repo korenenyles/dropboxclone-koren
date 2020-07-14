e7f1e6a4890a72b8ed5b8de55a32edaa5965e6fe
korenenyles <korenenyles@gmail.com>
2020-07-14 14:57:39 -0400

    added 404 and 500 custom error pages and functionality also worked on some routing

--- dropbox_file/urls.py
+++ dropbox_file/urls.py
@@ -4,10 +4,15 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', views.index_view, name="landingpage"),
+    path('filelist/', views.file_list_view, name="filelist")
+    
 
 ]
+
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
